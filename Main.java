9497cd244614f28dadb2738f8d629940239221b2
Onga22 <107556703+Onga22@users.noreply.github.com>
2022-06-19 18:22:29 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,42 @@
+package test;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Variables numericas
+        byte num1 = 1;
+        short num2 = 3;
+        int num3 = 55;
+        long num4 = 20500;
+
+        // Variables numericas con Decimales
+        float num5 = 55.68f;
+        double num6 = 88.99d;
+
+        // Variables booleanas
+        boolean estudia = false;
+        boolean trabaja = true;
+
+        // Variables texto
+        char letra = 'a';
+        String texto = "Hola";
+
+        //Imprimir variables
+        
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(estudia);
+        System.out.println(trabaja);
+        System.out.println(letra);
+        System.out.println(texto);
+        
+        
+        
+        
+    }
+}
